f763036311ba746ed4b796146e5c0eb7c61b5490
Ele <elemorelli@pm.me>
2020-12-14 00:55:06 -0300

    Basic details page

---
+++ src/components/VehicleDetailsPage.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
+import { get } from "../lib/fetch";
+
+import { populateVehicles } from "../actions/vehicles";
+
+import styles from "./VehicleDetailsPage.module.scss";
+
+const VehicleDetailsPage = () => {
+  const dispatch = useDispatch();
+  const { id } = useParams();
+  const vehicles = useSelector((state) => state.vehicles);
+  const [vehicle, setVehicle] = useState({});
+  const [stats, setStats] = useState({});
+
+  useEffect(async () => {
+    if (!vehicles) {
+      const response = await get("http://localhost:3000/vehicles");
+      dispatch(populateVehicles(response));
+      setVehicle(response.find((vehicle) => vehicle._id === id));
+    } else {
+      setVehicle(vehicles.find((vehicle) => vehicle._id === id));
+    }
+    const response = await get(`http://localhost:3000/vehicles/${id}/stats`);
+    setStats(response)
+  }, []);
+
+  return (
+    <div className="content-container">
+      <img height="250" width="250" alt="user profile avatar" src={`http://localhost:3000/vehicles/${id}/picture`} />
+      <div>
+        <p>Model: {vehicle.model}</p>
+        <p>Fuel Capacity: {vehicle.fuelCapacity}</p>
+        <p>averageDistanceBetweenFillUps: {stats.averageDistanceBetweenFillUps}</p>
+        <p>maxDistanceBetweenFillUps: {stats.maxDistanceBetweenFillUps}</p>
+        <p>nextFillUp: {stats.nextFillUp}</p>
+        <p>maxDistanceToFillUp: {stats.maxDistanceToFillUp}</p>
+        <p>averageFuelConsumption: {stats.averageFuelConsumption}</p>
+        <p>maxFuelConsumption: {stats.maxFuelConsumption}</p>
+        <p>minFuelConsumption: {stats.minFuelConsumption}</p>
+      </div>
+    </div>
+  );
+};
+
+export default VehicleDetailsPage;
+
+
+/*
+
+
+stats: {
+    : "877.52",
+    : "90081.50",
+    : "114261.92",
+    : "203465.90",
+    : "62.63",
+    : "6411.49",
+    : "0.69",
+  };
+
+*/
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Router, Route, Switch } from "react-router-dom";
 import { createBrowserHistory } from "history";
-import DashboardPage from "../components/DashboardPage";
-import NotFoundPage from "../components/NotFoundPage";
-import LoginPage from "../components/LoginPage";
+
 import PublicRoute from "./PublicRoute";
 import PrivateRoute from "./PrivateRoute";
+
+import LoginPage from "../components/LoginPage";
+import DashboardPage from "../components/DashboardPage";
+import VehicleDetailsPage from "../components/VehicleDetailsPage";
+import NotFoundPage from "../components/NotFoundPage";
 
 export const history = createBrowserHistory();
 
@@ -14,6 +17,7 @@ const AppRouter = () => (
     <Switch>
       <PublicRoute exact={true} path="/" component={LoginPage} />
       <PrivateRoute path="/dashboard" component={DashboardPage} />
+      <PrivateRoute path="/vehicles/:id" component={VehicleDetailsPage} />
       <Route component={NotFoundPage} />
     </Switch>
   </Router>
